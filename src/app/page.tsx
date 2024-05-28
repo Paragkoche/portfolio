ff930ae093147b5f564c56cf1752f8ae261ba780
Your Name <you@example.com>
2024-05-28 20:33:14 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Home/Hero";
+import TextAnimation from "@/components/ui/TextAnimation";
 import DotSpace from "@/components/ui/dotSpace";
 export default function Home() {
   return (
     <>
       <Hero />
       <DotSpace style={{ height: "25%" }} />
+      <TextAnimation paragraph="Partnering with tech innovators to craft software products adored by millions worldwide. Transforming code into magic with a splash of creativity and a lot of coffee!" />
     </>
   );
 }
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,19 +1,58 @@
+"use client";
 import "@/scss/home/hero.scss";
 import Button from "../ui/Button";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 const Hero = () => {
+  const body = useRef(null);
+
+  const isInView = useInView(body, { once: true, margin: "-75%" });
+
+  const animation = {
+    initial: { y: "150%" },
+
+    enter: (i: number) => ({
+      y: "0",
+      transition: {
+        duration: 0.75,
+        ease: [0.33, 1, 0.68, 1],
+        delay: 0.0075 * i,
+      },
+    }),
+  };
+  console.log(isInView);
+
   return (
-    <section className="hero_section">
+    <section className="hero_section" ref={body}>
       <div className="hero_section_header">
-        <h1>
+        <motion.h1
+          custom={0}
+          variants={animation}
+          initial="initial"
+          animate={"enter"}
+        >
           Full Stack <br />
-          <span>Web developer</span>
-        </h1>
+          <motion.span
+            custom={5}
+            variants={animation}
+            initial="initial"
+            animate={"enter"}
+          >
+            Web developer
+          </motion.span>
+        </motion.h1>
       </div>
       <div className="hero_section_info">
-        <p className="hero_section_info_main">
+        <motion.p
+          custom={2}
+          variants={animation}
+          initial="initial"
+          animate={"enter"}
+          className="hero_section_info_main"
+        >
           I'm Parag Koche, a Full Stack Web Developer transforming caffeine into
           dynamic, responsive web applications.
-        </p>
+        </motion.p>
         <span></span>
         <Button className="hero_section_info_other">view my work</Button>
       </div>
